5e87d8bada395f6841a1fc27c3f6e9df65806b88
Adeel.Tahir <Adeel.Tahir@cioxhealth.com>
2023-04-26 02:39:18 -0400

    Add comments and Png of the final frontend

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import plotly.express as px
 from backend import get_data
 
-
+# Add title, text input, slider, selectbox and subheader
 st.title("Weather Forcast for the next 5 days")
 place = st.text_input("Enter any place: ")
 days = st.slider("Forcast days", min_value=1,
@@ -11,9 +11,9 @@ days = st.slider("Forcast days", min_value=1,
 option = st.selectbox("Select data to view",
                       ("Temperature", "Sky view"))
 st.subheader(f"{option} for the next {days} days in {place}")
-
+# if place is not select
 if place:
-
+    # Get the temperate/sky view data with plot and images
     filtered_data = get_data(place, days)
 
     if option == "Temperature":
@@ -22,7 +22,7 @@ if place:
         figure = px.line(x=dates, y=temperature, labels={"x": "Date",
                                    "y": "Temperature (C)"})
         st.plotly_chart(figure)
-
+    # Mapping the images with the data using dict
     if option == "Sky view":
         images = {"Clear": "images/clear.png",
                   "Clouds": "images/cloud.png",
